66f16a2d3bfaf467f8ea23225c5ab93c73208d80
haroldZS <haroldzs1024@gmail.com>
2023-10-13 17:01:20 -0400

    MathClass object

---
+++ import-mathClass.js
@@ -0,0 +1,6 @@
+const MathClass = require("./mathClass");
+
+console.log(MathClass.addition(1, 2));
+console.log(MathClass.substract(2, 1));
+console.log(MathClass.multiplication(3, 2));
+console.log(MathClass.division(4, 2));
---
+++ mathClass.js
@@ -0,0 +1,8 @@
+const MathClass = {};
+
+MathClass.addition = (a, b) => a + b;
+MathClass.substract = (a, b) => a - b;
+MathClass.multiplication = (a, b) => a * b;
+MathClass.division = (a, b) => a - b;
+
+module.exports = MathClass;
